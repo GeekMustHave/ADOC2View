08635b3bb633f9ad95bcf6964b9651501582ae7e
GeekMustHave <john.schuster@phoenixworkgroup.com>
2019-03-20 11:30:46 -0400

    Got puppeteer to work 50% of the way

--- index.js
+++ index.js
@@ -1,16 +1,57 @@
 // --- ASCIIDoctor 2 Browser Viewer (ADOC2Viewer) - GeekMustHave
 
+
+const puppeteer = require('puppeteer');
 const fs = require('fs');
 
+// --- Load readme.adoc
+const adoc = require('asciidoctor.js')();
 
+// --- Read the ADOC file
+let adocFile = fs.readFileSync("./readme.adoc", 'utf8');
 
-const chromium = require('chromium');
-const {execFile} = require('child_process');
- 
-execFile(chromium.path, ['https://GeekMustHave.com'], err => {
-    console.log('Hello GeekMustHave!');
+// --- Convert to HTML using asciidoctor.js
+//  
+
+//let htmlContent = adoc.convert(adocFile, {'header_footer': true});
+let htmlContent = adoc.convertFile('./readme.adoc', {
+  'to_file': false, 
+  'header_footer': true, 
+  'safe': 'safe'
+//  ,'attributes': {'linkcss': true}
 });
 
+
+// --- Brings back a ablank screen
+//var htmlContent = adoc.loadFile('./readme.adoc');
+
+
+(async () => {
+  // --- Puppeteer launches Chromium in headless mode. To launch a full version of Chromium, set the 'headless' option when launching a browser:
+  const browser = await puppeteer.launch({
+    headless: false
+    ,args: [
+      '--start-maximized', 
+      '--windows-size=1910,1080', 
+      '--start-fullscreen',
+      '--disable-infobars'  // --- Remove 'Chrome is being controledd...' message
+    ]
+//    ,args: ['--start-fullscreen'  ]
+  });
+  const page = await browser.newPage();
+//  await page.setViewport({
+//    width: 0,
+//    height: 0,
+//    isLandscape: true
+//  }
+//  )
+  await page.setContent(htmlContent);
+
+  await page._client.send( 'Emulation.clearDeviceMetricsOverride' );
+  //await page.screenshot({path: 'example.png'});
+
+  //await browser.close();
+})();
 
 
 
@@ -21,6 +62,7 @@ execFile(chromium.path, ['https://GeekMustHave.com'], err => {
 
   DATE       Version Author   Description
 ==========  ======== ======   ====================================================================
+03/20/2019    V2.1c   JHRS    Took out Chromium and added puppeteer
 03/19/2019    V2.1b   JHRS    Initial brain fart
 
 
